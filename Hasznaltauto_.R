fb0bd3f285148f107bf0d6312f74eb8993b9f9f9
TimeaHunka <92434102+TimeaHunka@users.noreply.github.com>
2021-10-22 18:35:47 +0200

    Add files via upload

--- Hasznaltauto_.R
+++ Hasznaltauto_.R
@@ -33,7 +33,7 @@ library(plotly)
   
   df3$vetelar_atlag_mf <- df3$vetelar_atlag / 1000000
   
-   ggplot(df3 , aes(x=reorder(brand, vetelar_atlag_mf),
+ ábra1 <-   ggplot(df3 , aes(x=reorder(brand, vetelar_atlag_mf),
                     y=vetelar_atlag_mf , fill= evjarat, color =brand))+
     geom_boxplot()+
     geom_hline(aes(yintercept=mean(df3$vetelar_atlag_mf)))+
@@ -46,7 +46,7 @@ library(plotly)
     theme(legend.position = "none",
           axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
 
-  
+  ggplotly(ábra1)
   
   
   
@@ -89,8 +89,8 @@ by (audi$vetelar , audi$sebessegvalto_automata , sd , na.rm = T)
 psych::describeBy (audi$vetelar , audi$sebessegvalto_automata ,na.rm = T)
 
 # Hipotézis
-H0: mu <= automata
-H1: mu > automata
+#H0: mu automata <= mu manualis
+#H1: mu  automata > mu manualis
 
 ketmintas_t <- t.test (vetelar ~ sebessegvalto_automata , 
                        data = audi , 
@@ -115,4 +115,62 @@ ggplot (audi , aes (x = sebessegvalto_automata , y = vetelar)) +
   geom_jitter (alpha = 0.7 , shape = '.') +
   coord_cartesian ()
 
+
+
+
+
+
+
+
+
+
+
+audi_modell <-audi %>% 
+  group_by(modell) %>% 
+  nest()
+
+audi_a6 <- (audi_modell[[2]][[1]])
+
+audi_a6$sebessegvalto_automata <- ifelse(audi_a6$sebessegvalto_fajtaja == "automata" , 1 , 0 )
+
+#Normalitás vizsgálat
+
+by (audi_a6$vetelar , audi_a6$sebessegvalto_automata , shapiro.test)
+hist (audi_a6$vetelar [audi_a6$sebessegvalto_automata==1])
+
+#p<0.01 ezért szignifikánsan eltér az eloszlás a normálistól mindkét csoportnál. 
+#Viszont nagy mintaelemszám miatt ( n>100 ) ettől eltekinthetünk
+
+
+#Szoráshomogenitás
+
+by (audi_a6$vetelar , audi_a6$sebessegvalto_automata , sd , na.rm = T)
+psych::describeBy (audi_a6$vetelar , audi_a6$sebessegvalto_automata ,na.rm = T)
+
+# Hipotézis
+#H0: mu automata <= mu manualis
+#H1: mu  automata > mu manualis
+
+ketmintas_t <- t.test (vetelar ~ sebessegvalto_automata , 
+                       data = audi_a6 , 
+                       alternative = "greater" , 
+                       var.equal = T) 
+ketmintas_t
+
+df_t <-  ketmintas_t [["parameter"]][["df"]]
+t    <-  ketmintas_t [["statistic"]][["t"]]
+
+r <-  sqrt ( t^2 / (t^2 + df_t) )
+r
+
+
+#Közepes mértékű hatásról beszélhetünk.
+
+
+boxplot (vetelar ~ sebessegvalto_automata , data = audi_a6)
+
+ggplot (audi , aes (x = sebessegvalto_automata , y = vetelar)) +
+  stat_summary (fun.data = mean_cl_normal , geom = "errorbar" , color = "red" , width = 0.5) +
+  geom_jitter (alpha = 0.7 , shape = '.') +
+  coord_cartesian ()
 
